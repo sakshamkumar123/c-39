cd2bd51b4ae167305114f830665cae1145b009b4
KUMAR MANOJ MK <210333@mahindra.com>
2020-12-09 13:44:58 +0530

    image for all sprites

--- js/Game.js
+++ js/Game.js
@@ -30,9 +30,13 @@ class Game {
     }
 
     car1 = createSprite(100,200);
+    car1.addImage("car1",car1Img)
     car2 = createSprite(300,200);
+    car2.addImage("car2",car2Img)
     car3 = createSprite(500,200);
+    car3.addImage("car3",car3Img)
     car4 = createSprite(700,200);
+    car4.addImage("car4",car4Img)
     cars = [car1, car2, car3, car4];
   }
 
@@ -42,13 +46,15 @@ class Game {
     Player.getPlayerInfo();
     
     if(allPlayers !== undefined){
+      background("#c68767")
+      image(track,0,-displayHeight*4,displayWidth,displayHeight*5)
       //var display_position = 100;
       
       //index of the array
       var index = 0;
 
       //x and y position of the cars
-      var x = 0;
+      var x = 150;
       var y;
 
       for(var plr in allPlayers){
@@ -79,6 +85,17 @@ class Game {
       player.update();
     }
 
+    if (player.distance>3800){
+      gameState=2
+    }
+
+    
+
     drawSprites();
   }
+  end(){
+    console.log("game ended")
+  
+  }
 }
+
--- sketch.js
+++ sketch.js
@@ -9,6 +9,17 @@ var database;
 var form, player, game;
 
 var cars, car1, car2, car3, car4;
+var ground,track,car1Img,car2Img,car3Img,car4Img;
+
+function preload(){
+  track=loadImage("images/track.jpg")
+  car1Img=loadImage("images/car1.png")
+  car2Img=loadImage("images/car2.png")
+  car3Img=loadImage("images/car3.png")
+  car4Img=loadImage("images/car4.png")
+  ground = loadImage("images/ground.png")
+  
+}
 
 
 function setup(){
@@ -27,5 +38,8 @@ function draw(){
   if(gameState === 1){
     clear();
     game.play();
+  }
+  if (gameState===2){
+    game.end()
   }
 }
